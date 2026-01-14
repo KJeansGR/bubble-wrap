feefc3f08fab72d7aed86b66488d6dce87fa6766
WillyxWonka <kalif.edu@gmail.com>
2026-01-14 22:21:20 +0000

    added functionality to buttons, they now change background color

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,49 @@
 int main()
 {
     bn::core::init();
-    bn::backdrop::set_color(bn::color(20, 20, 31));
+    // original
+    float r = 20;
+    float g = 20;
+    float b = 31;
+
+    // a button
+    float x = 0;
+    float y = 0;
+    float z = 31;
+
+    // b button
+    float i = 0;
+    float j = 31;
+    float k = 0;
+
+    bn::backdrop::set_color(bn::color(r, g, b));
 
     while (true)
     {
-        if (bn::keypad::a_pressed())
+        // blend colors if a and b are pressed, limts the max value to 31
+        if ((bn::keypad::a_pressed() && bn::keypad::b_pressed()) ||
+            (bn::keypad::a_held() && bn::keypad::b_held()))
         {
-            bn::backdrop::set_color(bn::color(0, 0, 0));
+            float rT = x + i;
+            float gT = y + j;
+            float bT = z + k;
+            bn::backdrop::set_color(bn::color(std::min((int)rT, 31), std::min((int)gT, 31), std::min((int)bT, 31)));
         }
-        if (bn::keypad::b_held())
+        // sets to defined a color
+        else if (bn::keypad::a_pressed() || bn::keypad::a_held())
         {
-            bn::backdrop::set_color(bn::color(255, 255, 255));
+            bn::backdrop::set_color(bn::color(x, y, z));
         }
-
+        // sets to defined b color
+        else if (bn::keypad::b_pressed() || bn::keypad::b_held())
+        {
+            bn::backdrop::set_color(bn::color(i, j, k));
+        }
+        // revert to original color if nothing is pressed
+        else if (!bn::keypad::a_pressed() && !bn::keypad::b_pressed())
+        {
+            bn::backdrop::set_color(bn::color(r, g, b));
+        }
         bn::core::update();
     }
 }
