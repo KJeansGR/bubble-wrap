f3f7057fb30bbee3661122e2a02960312d039270
WillyxWonka <kalif.edu@gmail.com>
2026-01-14 21:16:15 +0000

    added minimal button interaction that changes background color

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
 
 int main()
 {
@@ -9,6 +10,15 @@ int main()
 
     while (true)
     {
+        if (bn::keypad::a_pressed())
+        {
+            bn::backdrop::set_color(bn::color(0, 0, 0));
+        }
+        if (bn::keypad::b_held())
+        {
+            bn::backdrop::set_color(bn::color(255, 255, 255));
+        }
+
         bn::core::update();
     }
 }
